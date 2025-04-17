223f6687472566dd29f58d8dbdfdf5bace710a71
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2025-04-17 16:12:59 -0400

    EPICA 2 - feat: permits to edit a property

--- src/components/details/PropertyCard.tsx
+++ src/components/details/PropertyCard.tsx
@@ -1,6 +1,10 @@
+"use client";
+import { MdModeEditOutline } from "react-icons/md";
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+
 import styles from '../../app/config/theme/Card.module.css';
 import { Colors } from "@/app/config/theme/Colors";
-import { MdModeEditOutline } from "react-icons/md";
 
 interface Property {
   state: string;
@@ -12,20 +16,38 @@ interface PropertyCardProps {
   property: Property;
 }
 
-const PropertyCard: React.FC<PropertyCardProps> = ({ onEditClick, property }) => (
-  <div className={styles.card}>
-    <div className={styles.propertyCardHeader}>
-      <span className={styles.propertyCardTitle}>Detalles del lote</span>
-      <button className={styles.propertyCardEditBtn} onClick={onEditClick}>
-        <MdModeEditOutline style={{ color: Colors.text_color, fontSize: "24px", cursor: "pointer" }} />
-        Editar
-      </button>
+interface JwtPayload {
+  role: string;
+}
+
+const PropertyCard: React.FC<PropertyCardProps> = ({ onEditClick, property }) => {
+  const [role, setRole] = useState<string | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decoded = jwtDecode<JwtPayload>(token);
+      setRole(decoded.role);
+    }
+  }, []);
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.propertyCardHeader}>
+        <span className={styles.propertyCardTitle}>Detalles del lote</span>
+        {role === "admin" && (
+          <button className={styles.propertyCardEditBtn} onClick={onEditClick}>
+            <MdModeEditOutline style={{ color: Colors.text_color, fontSize: "24px", cursor: "pointer" }} />
+            Editar
+          </button>
+        )}
+      </div>
+      <ul>
+        <li><span>Estado: </span><span>{property.state}</span></li>
+        <li><span>Precio (DÓLARES): </span><span>{property.price}</span></li>
+      </ul>
     </div>
-    <ul>
-      <li><span>Estado: </span><span>{property.state}</span></li>
-      <li><span>Precio (DÓLARES): </span><span>{property.price}</span></li>
-    </ul>
-  </div>
-);
+  );
+};
 
 export default PropertyCard;
