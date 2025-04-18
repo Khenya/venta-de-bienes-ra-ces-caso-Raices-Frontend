d425134bf5a36523c46a637e82ffba89b574843f
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2025-04-18 17:39:35 -0400

    EPICA 2 - feat: permits to add customer

--- src/components/details/AdjudicatorCard.tsx
+++ src/components/details/AdjudicatorCard.tsx
@@ -1,3 +1,7 @@
+'use client';
+import { useEffect, useState } from "react";
+import {jwtDecode} from "jwt-decode";
+
 import styles from '../../app/config/theme/Card.module.css';
 
 interface Customer {
@@ -9,37 +13,48 @@ interface Customer {
 interface AdjudicatorCardProps {
   onAddClick: () => void;
   customer?: Customer | null;
-  allowAdd?: boolean;
 }
 
-const AdjudicatorCard: React.FC<AdjudicatorCardProps> = ({ onAddClick, customer }) => (
-  <div className={styles.card}>
-    <div className={styles.propertyCardHeader}>
-      <span className={styles.propertyCardTitle}>Adjudicatario</span>
-    </div>
-    
-    {customer ? (
-      <div className="space-y-2">
-        <p><span className="font-semibold">Nombre:</span> {customer.name}</p>
-        <p><span className="font-semibold">CI:</span> {customer.ci}</p>
-        <p><span className="font-semibold">Teléfono:</span> {customer.phone}</p>
+const AdjudicatorCard: React.FC<AdjudicatorCardProps> = ({ onAddClick, customer }) => {
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decoded: any = jwtDecode(token);
+      setIsAdmin(decoded?.role === "admin");
+    }
+  }, []);
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.propertyCardHeader}>
+        <span className={styles.propertyCardTitle}>Adjudicatario</span>
       </div>
-    ) : (
-      <>
-  <div className={styles.emptyAdjudicatorContainer}>
-    <button 
-      onClick={onAddClick}
-      className={styles.adjudicatorButton}
-    >
-      Agregar un adjudicatario
-    </button>
-    <p className={styles.adjudicatorHint}>
-      Para agregar un adjudicatario debes cambiar el estado a "RESERVADO" o "CANCELADO"
-    </p>
-  </div>
-      </>
-    )}
-  </div>
-);
+      
+      {customer ? (
+        <div className="space-y-2">
+          <p><span className="font-semibold">Nombre:</span> {customer.name}</p>
+          <p><span className="font-semibold">CI:</span> {customer.ci}</p>
+          <p><span className="font-semibold">Teléfono:</span> {customer.phone}</p>
+        </div>
+      ) : (
+        isAdmin && (
+          <div className={styles.emptyAdjudicatorContainer}>
+            <button 
+              onClick={onAddClick}
+              className={styles.adjudicatorButton}
+            >
+              Agregar un adjudicatario
+            </button>
+            <p className={styles.adjudicatorHint}>
+              Para agregar un adjudicatario debes cambiar el estado a "RESERVADO" o "CANCELADO"
+            </p>
+          </div>
+        )
+      )}
+    </div>
+  );
+};
 
 export default AdjudicatorCard;
