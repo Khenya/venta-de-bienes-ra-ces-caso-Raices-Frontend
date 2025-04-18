fc0556c17aab6b86761b653016189883deeea158
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2025-04-18 08:59:07 -0400

    EPICA 5 - feat: map

--- src/pages/Plano.tsx
+++ src/pages/Plano.tsx
@@ -1,18 +1,34 @@
 "use client";
+import Image from "next/image";
+
 import withAuth from '../hoc/WithAuth';
 import Header2 from "@/components/common/Header_2";
+import map from "../assetes/map.png";
 
 const PlanoPage = () => {
   return (
-    <div className="min-h-screen">
-      <Header2/>
-      <main style={{ 
-        paddingTop: "80px", 
-        minHeight: "calc(100vh - 80px)"
-      }}>
-        <h1 className="text-3xl font-bold text-center">
-          ¡Bienvenido a la página de Mapa!
-        </h1>
+    <div className="min-h-screen overflow-x-hidden bg-white">
+      <Header2 />
+      <main
+        style={{
+          paddingTop: "90px",
+          minHeight: "calc(100vh - 80px)",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+        }}
+      >
+        <div style={{ maxWidth: "100%", padding: "0 1rem" }}>
+          <Image
+            src={map}
+            alt="map"
+            style={{
+              width: "100%",
+              height: "auto",
+              display: "block",
+            }}
+          />
+        </div>
       </main>
     </div>
   );
